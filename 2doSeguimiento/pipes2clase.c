4e20ed17f0e6bb132459e2f22a73537f884792a6
Samir Mejia <samir04m@gmail.com>
2018-09-29 13:22:00 -0500

    Commit 11

--- 2doSeguimiento/pipes2clase.c
+++ 2doSeguimiento/pipes2clase.c
@@ -2,56 +2,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h> 
+#include <wait.h>
+#include <sys/types.h>
 
 int main(){
-    pid_t childs[3], padre = getpid();
+    pid_t hijo, padre = getpid();
     int fd1[2], fd2[2], i, n;
-    char buffer_out[1024], buffer_in[1024];
+    char buffer[1024];
 
-    pipe(fd1);
-    pipe(fd2);
+    if (pipe(fd1)) exit(-1);
+    if (pipe(fd2)) exit(-1);
 
     for (i=0; i<3; i++){
-        if (childs[i] = fork()) break;
+        if (hijo = fork()) break;
     }
 
     if (getpid() != padre){
-        if (i == 0 && childs[0] == 0){
+        if (i == 1){
             close(fd1[0]);
             close(fd2[0]);
             close(fd2[1]);
-            strcpy(buffer_out, "Vengo del proceso i=1");
-            write(fd1[1], buffer_out, strlen(buffer_out));
+            strcpy(buffer, "Vengo del primer proceso hijo");
+            write(fd1[1], buffer, strlen(buffer));
             close(fd1[1]);
         }
-        if (i == 1 && childs[1] == 0){
+        if (i == 2){
             close(fd1[1]);
             close(fd2[0]);
-            n = read(fd1[0], buffer_in, sizeof(buffer_out));
-            buffer_in[n] = '\0';
-            write(fd2[1], buffer_in, strlen(buffer_in));
+            n = read(fd1[0], buffer, sizeof(buffer));
+            buffer[n] = '\0';
+            write(fd2[1], buffer, strlen(buffer));
             close(fd1[0]);
             close(fd2[1]);
         }
-        if (i == 2 && childs[2] == 0){
+        if (i == 3){
             close(fd1[0]);
             close(fd1[1]);
             close(fd2[1]);
-            n = read(fd2[0], buffer_in, sizeof(buffer_out));
-            buffer_in[n] = '\0';
-            printf("Process %d: Leido: %s \n", getpid(), buffer_in);
+            n = read(fd2[0], buffer, sizeof(buffer));
+            buffer[n] = '\0';
+            printf("Process %d: Leido: %s \n", getpid(), buffer);
             close(fd2[0]);
         }
-
     }else{
         close(fd1[0]);
         close(fd1[1]);
         close(fd2[0]);
         close(fd2[1]);
     }
-
-
 
     if (padre==getpid()){
         char b[500];
