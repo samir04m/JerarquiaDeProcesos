d50b800be04f21a33cd157135850a0ddaaf6353e
Samir Mejia <samir04m@gmail.com>
2018-10-01 02:12:19 -0500

    Commit 17 (esta lista)

--- 2doSeguimiento/operaciones_cada_proceso_archivos.c
+++ 2doSeguimiento/operaciones_cada_proceso_archivos.c
@@ -27,7 +27,6 @@ int main()
     if (getpid() != padre){
 
         if (j > 0){
-            // printf("%d hijo i=%d j=%d\n", getpid(), i, j);
             for (int k=0; k<npipes; k++){
                 if ((j+2) != k) close(fd[k][0]);
                 if ((j+3) != k) close(fd[k][1]);
@@ -39,26 +38,51 @@ int main()
             }
         }
 
-        int TAM;
+        int TAM, c=0;
 
         if (j > 0){
             read(fd[j+2][0], &TAM, sizeof(int));
             char info[TAM];
-            n = read(fd[j+2][0], info, TAM);
-            info[n] = '\0';
-            printf("Process %d: leido: %s\n", getpid(), info);
+
+            for (int k=0; k<TAM; k++){
+                read(fd[j+2][0], &info[k], sizeof(char));
+                if (j == 1) if (info[k] == '.') c++;
+                if (j == 2) if (info[k] == ',') c++;
+            }
+            if (j == 1)
+                printf("proceso %d: Hay %d punto(s)\n", getpid(), c);
+            if (j == 2)
+                printf("proceso %d: Hay %d coma(s)\n", getpid(), c);
+
             write(fd[j+3][1], &TAM, sizeof(int));
-            write(fd[j+3][1], info, TAM);
+            for (int k=0; k<TAM; k++){
+                write(fd[j+3][1], &info[k], sizeof(char));
+            }
             close(fd[j+2][0]);
             close(fd[j+3][1]);
         }else{
             read(fd[i][0], &TAM, sizeof(int));
             char info[TAM];
-            n = read(fd[i][0], info, TAM);
-            info[n] = '\0';
-            printf("Process %d: leido: %s\n", getpid(), info);
+
+            for (int k=0; k<TAM; k++){
+                read(fd[i][0], &info[k], sizeof(char));
+                if (i == 0) if (info[k] >= 'A' && info[k] <= 'Z') c++;
+                if (i == 1) if (info[k] >= 'a' && info[k] <= 'z') c++;
+                if (i == 2) if (info[k] == ' ') c++;
+            }
+
+            if (i == 0)
+                printf("proceso %d: Hay %d mayuscula(s)\n", getpid(), c);
+            if (i == 1)
+                printf("proceso %d: Hay %d minuscula(s)\n", getpid(), c);
+            if (i == 2)
+                printf("proceso %d: Hay %d espacio(s)\n", getpid(), c);
+
             write(fd[i+1][1], &TAM, sizeof(int));
-            write(fd[i+1][1], info, TAM);
+            for (int k=0; k<TAM; k++){
+                write(fd[i+1][1], &info[k], sizeof(char));
+            }
+
             close(fd[i][0]);
             close(fd[i+1][1]);
         }
@@ -70,11 +94,26 @@ int main()
             if (k != 5) close(fd[k][0]);
             close(fd[k][1]);
         }
-        char buffer[1024];
-        strcpy(buffer, "Hola soy mensaje");
-        int TAM = strlen(buffer);
+        FILE *archivo = fopen("caracteres2.txt", "r");
+        if (!archivo) {
+            printf("Erro al abrir el archivo\n");
+            exit(-1);
+        }
+        fseek(archivo, 0, SEEK_END);
+        int TAM = ftell(archivo);
+        fseek(archivo, 0, 0);
+        TAM -= 1;
+        char info[TAM];
+
+        for (int k=0; k<TAM; k++){
+            info[k] = getc(archivo);
+        }
+        fclose(archivo);
+
         write(fd[0][1], &TAM, sizeof(int));
-        write(fd[0][1], buffer, TAM);
+        for (int k=0; k<TAM; k++){
+            write(fd[0][1], &info[k], sizeof(char));
+        }
         close(fd[0][1]);
     }
 
