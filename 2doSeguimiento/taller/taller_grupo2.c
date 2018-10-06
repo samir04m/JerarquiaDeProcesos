2749790f4e542bbe6bedbfb86c0faf50508e32f4
Samir Mejia <samir04m@gmail.com>
2018-10-06 18:21:50 -0500

    Commit 20 (tallerg2 listo)

--- 2doSeguimiento/taller/taller_grupo2.c
+++ 2doSeguimiento/taller/taller_grupo2.c
@@ -4,7 +4,7 @@
 #include <wait.h>
 #include <string.h>
 
-int main(){
+int main(int argc, char *argv[]){
     system("clear");
     pid_t childs[2], root = getpid();
     int i, fd1[2], fd2[2];
@@ -18,6 +18,7 @@ int main(){
         if(i == 0 && childs[0] == 0){
             close(fd1[0]);
             close(fd2[0]);
+            int fsize;
 
             FILE *file1 = fopen("file1.txt", "r");
             if (!file1){
@@ -25,23 +26,52 @@ int main(){
                 exit(-1);
             }
             fseek(file1, 0, SEEK_END);
-            int fsize = ftell(file1);
+            fsize = ftell(file1);
             fseek(file1, 0, 0);
 
-            int nlines = 0, lsize;
+            int lsize;
             char *buffer;
             buffer = malloc((fsize)*sizeof(char));
 
-
             while(fgets(buffer, fsize, file1)){
-                nlines++;
                 buffer[strlen(buffer)-1] = '\0'; //cambio el salto de linea por el final de linea
                 lsize = strlen(buffer);
-                printf("%s\n", buffer);
+                write(fd1[1], &lsize, sizeof(int));
+                write(fd1[1], buffer, lsize);
             }
-            printf("nlines %d\n", nlines);
-
             fclose(file1);
+
+            strcpy(buffer, "<<end1>>");
+            lsize = strlen(buffer);
+            write(fd1[1], &lsize, sizeof(int));
+            write(fd1[1], buffer, lsize);
+
+
+            FILE *file2 = fopen("file2.txt", "r");
+            if (!file2){
+                printf("Erro al abrir el archivo file2\n");
+                exit(-1);
+            }
+            fseek(file2, 0, SEEK_END);
+            fsize = ftell(file2);
+            fseek(file2, 0, 0);
+
+            buffer = malloc((fsize)*sizeof(char));
+
+            while(fgets(buffer, fsize, file2)){
+                buffer[strlen(buffer)-1] = '\0'; //cambio el salto de linea por el final de linea
+                lsize = strlen(buffer);
+                write(fd2[1], &lsize, sizeof(int));
+                write(fd2[1], buffer, lsize);
+            }
+            fclose(file2);
+
+            strcpy(buffer, "<<end2>>");
+            lsize = strlen(buffer);
+
+            write(fd2[1], &lsize, sizeof(int));
+            write(fd2[1], buffer, strlen(buffer));
+
             close(fd1[1]);
             close(fd2[1]);
         }
@@ -49,6 +79,40 @@ int main(){
             close(fd1[1]);
             close(fd2[1]);
 
+            int lsize, sw = 0, end1=0, end2=0;
+            char *buffer1, *buffer2;
+
+            while(1){
+                if (!end1){
+                    read(fd1[0], &lsize, sizeof(int));
+                    buffer1 = malloc((lsize)*sizeof(char));
+                    read(fd1[0], buffer1, lsize);
+
+                    if (!strcmp(buffer1, "<<end1>>")){
+                        end1 = 1;
+                        if (sw == 1) break;
+                        else sw++;
+                    }else{
+                        for (int a=0; a<lsize; a++) printf("%c", buffer1[a]);
+                        printf("\n");
+                    }
+                }
+
+                if (!end2){
+                    read(fd2[0], &lsize, sizeof(int));
+                    buffer2 = malloc((lsize)*sizeof(char));
+                    read(fd2[0], buffer2, lsize);
+
+                    if (!strcmp(buffer2, "<<end2>>")){
+                        end2 = 1;
+                        if (sw == 1) break;
+                        else sw++;
+                    }else{
+                        for (int a=0; a<lsize; a++) printf("%c", buffer2[a]);
+                        printf("\n");
+                    }
+                }
+            }
 
             close(fd1[0]);
             close(fd2[0]);
